a5545569ef6b4a598ed2eb1068e8cbd3ca2bba9b
alyak <alya.korobeynikova99@gmail.com>
2024-03-21 13:22:08 +0300

    0.5

--- app/src/main/java/com/korobeynikova/pr7_primer/MainActivity.kt
+++ app/src/main/java/com/korobeynikova/pr7_primer/MainActivity.kt
@@ -7,7 +7,12 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+    private var right = 0
+    private var lose = 0
+    private var all = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,8 @@ class MainActivity : AppCompatActivity() {
             binding.check.isEnabled = false
             binding.start.isEnabled = true
             binding.vvod.isEnabled = false
+
+            checkPrimer()
         }
     }
     private fun generateRandomOperand(): Int {
@@ -43,5 +50,30 @@ class MainActivity : AppCompatActivity() {
         binding.nullNull1.text = generateRandomOperand().toString()
         binding.nullNull2.text = generateRandomOperand().toString()
         binding.znak.text = generateRandomOperator().toString()
+    }
+    private fun checkPrimer(){
+        val numberOne = binding.nullNull1.text.toString().toInt()
+        val numberYwo = binding.nullNull2.text.toString().toInt()
+
+        val result = when (binding.znak.text) {
+            "+" -> numberOne + numberYwo
+            "-" -> numberOne - numberYwo
+            "*" -> numberOne * numberYwo
+            "/" -> numberOne / numberYwo
+            else -> throw IllegalArgumentException("Unknown operator")
+        }
+
+        if (result == binding.vvod.text.toString().toInt()){
+            right++
+        }
+        else{
+            lose++
+        }
+        all++
+        val present = String.format("%.2f%%", (right.toDouble() / all.toDouble()) * 100)
+        binding.null1.text = all.toString()
+        binding.null2.text = right.toString()
+        binding.null3.text = lose.toString()
+        binding.prosenttext.text = present
     }
 }
